14c97297e1d4ca5e9160493557419d69d2646ca0
Ricky Kuo <rickykuo925@gmail.com>
2023-03-03 15:06:30 +0000

    grab text done

--- main.py
+++ main.py
@@ -54,38 +54,44 @@ class DownloadBot:
         
         # do all the download operations
         row_size = len(item_rows)
+        name = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='main3']/div[1]/div/div[1]/div[1]/b"))).text
+        id = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[1]/td[3]").text.split(' ')[0]
         
         if row_size == 4:
-            name = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='main3']/div[1]/div/div[1]/div[1]/b"))).text
-            id = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[1]/td[3]").text
             size = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[3]/td[2]").text
             era = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[4]/td[2]").text
-            time.sleep(1)
-            ITEM = {
-                "id": id,
-                "name": name,
-                "size": size,
-                "era": era
-            }
-            self.item_list.append(ITEM)
+            desc = ''
+            
         elif row_size == 5:
-            name = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='main3']/div[1]/div/div[1]/div[1]/b"))).text
-            pass
+            condition = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[2]/td[1]").text
+            
+            if condition == '分類':
+                size = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[3]/td[2]").text
+                era = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[4]/td[2]").text
+                desc = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[5]/td[2]").text
+                
+            elif condition == '品名':   
+                size = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[4]/td[2]").text
+                era = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[5]/td[2]").text
+                desc = ''
+                
+            else:
+                print("something I didn't recognize yet") 
+            
         elif row_size == 6: 
-            name = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='main3']/div[1]/div/div[1]/div[1]/b"))).text
-            id = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[1]/td[3]").text
             size = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[4]/td[2]").text
             era = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[5]/td[2]").text
             desc = self.driver.find_element(By.XPATH, "//*[@id='collapseExample']/div/div[1]/table/tbody/tr[6]/td[2]").text
-            time.sleep(1)
-            ITEM = {
-                "id": id,
-                "name": name,                
-                "size": size,
-                "era": era,
-                "desc": desc
-            }
-            self.item_list.append(ITEM)
+            
+        time.sleep(1)
+        ITEM = {
+            "id": id,
+            "name": name,                
+            "size": size,
+            "era": era,
+            "desc": desc
+        }
+        self.item_list.append(ITEM)
             
         # close the item tab, and switch to main tab
         self.driver.close()
